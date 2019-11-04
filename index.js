bf14ce7a62bebce7c5cc8f2a01067992ec2fa245
kmlee6 <32027946+kmlee6@users.noreply.github.com>
2019-11-04 14:58:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
+
 //TODO
+console.log(Array.isArray(a))
+console.log(Array.isArray(b))
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
+console.log(a.map((value)=>{return value*2}))
 // TODO should output [2,4,6,8,10]
 
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
+console.log(colors.join(" "))
 // case 2 output: 'Red+Green+White+Black'
+console.log(colors.join("+"))
 // case 3 output: 'Red,Green,White,Black'
+console.log(colors.join(","))
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+console.log(a.sort((a,b)=>{return b-a}))
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+console.log(a.sort((x,y)=>{return a.filter(item=>item===x).length-a.filter(item=>item===y).length}).pop())
